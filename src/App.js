54c302c4a2d1b7c984c9bb69d72e5ef646ab1192
Eddieklai <eddie.klai@epitech.eu>
2022-08-16 11:14:04 +0200

    [Setup begin project]

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function App() {
+  return (
+    <Container className="App">
+
+    </Container>
+  );
+}
+
+const Container = styled.div `
+    
+`;
